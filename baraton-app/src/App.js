d75cd289551ec5b91405b9a992f2d6e68c26c6f1
Jhon Gomez <jhon.alx018@gmail.com>
2018-08-25 10:46:42 -0400

    fixing tree folder for start app

--- baraton-app/src/App.js
+++ baraton-app/src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import { TopBar } from './Components/TopBar/TopBar';
 import './assets/App.css';
 import { Provider } from 'react-redux';
 import { store } from './Store/Store';
+import BodyApp from './Components/BodyApp/BodyApp';
 
 class App extends Component {
-  componentWillMount() {
-    console.log(store);
-  }
-
   render() {
     return (
       <div className="App">
         <Provider store={store}>
-          <TopBar />
+          <BodyApp />
         </Provider>
       </div>
     );
---
+++ baraton-app/src/Components/BodyApp/BodyApp.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import { TopBar } from '../TopBar/TopBar';
+
+class BodyApp extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+      </div>
+    );
+  }
+}
+
+export default BodyApp;
+
