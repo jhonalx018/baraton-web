6db348f6c4e684686b3b7250cf5517d295b3d4c4
Jhon Gomez <jhon.alx018@gmail.com>
2018-08-28 13:08:53 -0400

    filters by min-max price | name done

--- baraton-app/src/Components/ContentProducts/ContentProducts.js
+++ baraton-app/src/Components/ContentProducts/ContentProducts.js
@@ -1,6 +1,38 @@
 import React, {Component} from 'react';
 import '../../assets/ContentProducts/ContentProducts.css';
 import {connect} from 'react-redux';
+import Input from '@material-ui/core/Input';
+import TextField from '@material-ui/core/TextField';
+
+const SearchBar = props => (
+
+		<div className="search-bar">
+				<div
+						className="products-label"
+						style={{
+						'textAlign': 'left',
+						'padding': '0px 10px 10px',
+						'cursor': 'pointer'
+				}}>Filters</div>
+				<Input
+						type="text"
+						onChange={props.eventKey}
+						className="inputSearchBar"
+						placeholder="Filter by name and quantity"/>
+				<div className="content-form-control">
+						<TextField
+								label="Min Price"
+								type="number"
+								className="number-text-field"
+								onChange={props.minPrice}/>
+						<TextField
+								label="Max Price"
+								type="number"
+								className="number-text-field"
+								onChange={props.maxPrice}/>
+				</div>
+		</div>
+);
 
 const ProduxBox = (data) => {
 
@@ -24,7 +56,11 @@ class ContentProducts extends Component {
 		constructor() {
 				super();
 				this.actives = [];
+				this.maxPrice = 0;
+				this.minPrice = 0;
 				this.state = {
+						valSearch: '',
+						orginalData: [],
 						products: [],
 						categoriesSelected: []
 				}
@@ -60,24 +96,76 @@ class ContentProducts extends Component {
 																		.push(parseInt(i));
 														}
 												}
-												if (context.actives.length > 0 && data.datosFiltrados != undefined) {
-
-														let dataClean = dataProducts.filter(item => context.validateItemCategorie(item));
-														context.setState({products: dataClean});
-												} else {
-														context.setState({products: dataProducts});
-												}
+												let dataClean = dataProducts.filter(item => context.validateItemCategorie(item));
+												let dataFull = (context.actives.length > 0 && data.datosFiltrados != undefined)
+														? dataClean
+														: dataProducts;
+												context.setState({products: dataProducts, orginalData: dataFull});
+												data
+												context.filterComplety();
 
 										});
 						});
 		}
 
+		filterKey = (event) => {
+				this.setState({
+						valSearch: event.target.value
+				}, () => {
+						this.filterComplety();
+				})
+		}
+
+		filterComplety = () => {
+
+				const products = this.state.orginalData;
+				const elements = products.filter(item => (item.quantity.toString().toLowerCase().indexOf(this.state.valSearch.toLowerCase()) != -1 || item.name.toString().toLowerCase().indexOf(this.state.valSearch.toLowerCase())) != -1);
+				let finalElements = elements;
+
+				if (this.minPrice > 0) {
+						finalElements = elements.filter(item => this.parseToNumber(item.price) >= this.minPrice);
+				}
+
+				if (this.maxPrice > 0) {
+						finalElements = finalElements.filter(item => this.maxPrice >= this.parseToNumber(item.price));
+				}
+				this.setState({products: finalElements});
+
+		}
+
+		maxPriceFunction = (event) => {
+				this.maxPrice = this.parseToNumber(event.target.value);
+				this.filterComplety();
+		}
+
+		minPriceFunction = (event) => {
+				this.minPrice = this.parseToNumber(event.target.value);
+				this.filterComplety();
+		}
+
+		parseToNumber = (numberString = 0) => {
+				let arrayLetters = numberString.split('');
+
+				let parseArr = arrayLetters.filter(item => (item.indexOf(',') === -1 && (item.indexOf('$') === -1) && (item.indexOf('.') === -1)));
+				let finalString = '';
+				parseArr.map(item => {
+						finalString = finalString + item;
+				});
+
+				return parseInt(finalString);
+		}
+
 		render() {
 				return (
 						<div className="App">
+
 								<h3 className="title-products">Product List</h3>
-								<div className="products-quantity">Quantity({this.state.products.length})</div>
+								<div className="products-label">Quantity({this.state.products.length})</div>
 								<div className="content-data">
+										<SearchBar
+												eventKey={this.filterKey}
+												maxPrice={this.maxPriceFunction}
+												minPrice={this.minPriceFunction}/>
 										<ProduxBox data={this.state.products}/>
 								</div>
 						</div>
