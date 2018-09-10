d8888766348001ddf9b8f7122d65a9d073202ff5
Jhon Gomez <jagomez@teravisiontech.com>
2018-09-10 14:22:09 -0400

    adding final changes

--- baraton-app/src/Store/Reduxers/ProductsRx.js
+++ baraton-app/src/Store/Reduxers/ProductsRx.js
@@ -1,12 +1,44 @@
 import { ServiceProducts } from '../../WebServices/ServiceProducts';
+import { element } from 'prop-types';
+
+const replaceQuantity = (data) => {
+  const prevBuy = (localStorage.getItem('orders'))
+  ? JSON.parse(localStorage.getItem('orders'))
+  : [];
+  
+  const result = data.map((item) => {
+
+    const res = prevBuy.map((itemEl) => {
+    
+      let filtEl  = itemEl.elements.filter((elm) => {
+        
+        if(elm.id === item.id){
+           console.log('aqui');
+           return elm;
+        }
+      });  
+      
+      if(filtEl.length > 0){
+        
+        item.quantity = parseInt(item.quantity) - parseInt(filtEl[0].carQuantity);
+        item.available = (item.quantity === 0) ? false: true;
+      }
+
+    });
+    return item;
+
+  }); 
+  return result;
+};
 
 const ProductsRx = async (state = [], action) => {
   switch (action.type) {
     case 'GET_DATA_PRODUCTS':
-      const data = await ServiceProducts();
+      let data = await ServiceProducts();
+      let datos = replaceQuantity(data)
       return [
         ...state,
-        ...data,
+        ...datos,
       ];
       break;
     case 'SET_FILTER_CATEGORIE':
